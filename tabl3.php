e10e6fad9171cd834637296157c3a82c72b97327
tedclass <76146058+tedclass@users.noreply.github.com>
2021-01-13 20:33:28 +0800

    Update tabl3.php
    added php stuff

--- tabl3.php
+++ tabl3.php
@@ -31,6 +31,21 @@
 <th>Menara Standard Chartered, TPM, Knee deep in pussay</th>
 <th>you know my number</th>
  </tr>
+  <?php
+  //this establishes connection to database server
+  $connectionInfo = array("UID" => "adminsimpleweb", "pwd" => "Heckno2u4", "Database" => "simplewebDatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:simplewebtp0500890.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+if(!$conn)
+{
+    die("Error connection: ".sqlsrv_errors());
+}
+else
+{
+    echo "<script>alert( 'Success!');</script>";
+}
+  
+  ?>
  </table>
  </center>
  </body>
